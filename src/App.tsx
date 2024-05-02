cda4a5e9db50e854c7a2398c1d0f43c6a5b0dbe0
harsh-dev-066 <harshthakur066@gmail.com>
2024-05-02 16:29:04 +0530

    project-setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="root">
+      {/* Let Panel - Sidebar */}
+      <div className="sidebar-wrapper">Sidebar</div>
+      {/* Right Panel */}
+      <div className="taxonomy-wrapper">Taxonomy</div>
+    </div>
+  );
+}
+
+export default App;
